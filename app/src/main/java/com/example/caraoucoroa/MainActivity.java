374d2c1094038594c526d074dc04ac82a9f66d43
Tiago <t.vp@hotmail.com>
2022-10-13 20:57:23 -0300

    Finalizando o app Cara e Coroa feito com o curso do Jamilton Damasceno

--- app/src/main/java/com/example/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/example/caraoucoroa/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.caraoucoroa.databinding.ActivityMainBinding;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+  private ActivityMainBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    View view = binding.getRoot();
+    setContentView(view);
+
+    binding.buttonJogar.setOnClickListener(v -> {
+      Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+
+      //Passar os dados para a próxima tela
+      int numero = new Random().nextInt(2); //0 e 1
+      intent.putExtra("numero", numero);
+
+      startActivity(intent);
+    });
+
   }
 }
